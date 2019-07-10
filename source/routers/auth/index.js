76724b408afd7918679f6b684fca366bc8978621
volodymyrmordas <vmordas@alteryx.com>
2019-07-11 00:48:03 +0300

    postment collection

--- source/routers/auth/index.js
+++ source/routers/auth/index.js
@@ -10,6 +10,7 @@ export const router = express.Router();
 router.post('/login', [ limiter(5, 60 * 1000) ], login);
 router.post('/logout', [ authenticate, limiter(5, 60 * 1000) ], logout);
 
+// todo: for test https://www.getpostman.com/collections/393548c161e374be8440
 router.post('/auth/login', [ limiter(5, 60 * 1000) ], loginCookies);
 
 export { router as auth };
